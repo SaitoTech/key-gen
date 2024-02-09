245c519346bcfc79cf3caa0c66caae45eeb80dd7
Umair Khan <umair@saito.tech>
2024-02-09 08:45:59 +0500

    Restructuring, removing unecassary packages, debug and fix erros

--- form.js
+++ form.js
@@ -1,72 +1,94 @@
 const secp256k1 = require('secp256k1');
 const { randomBytes } = require('crypto');
 const Crypto = require('./crypto.js')
-const crypto = Crypto();
 const qrcode = require('qrcode-generator');
 const bip39 = require('bip39');
 const tabs = require('tabs');
-const pbkdf2 = require('pbkdf2').pbkdf2Sync
-const unorm = require('unorm')
+const pbkdf2 = require('pbkdf2').pbkdf2Sync;
+const unorm = require('unorm');
 
 
 ////////////
 // Render //
 ////////////
-$(document).ready(function () {
-  var container = document.querySelector('.tab-container')
-  tabs(container);
-  new ClipboardJS('.copy');
-});
+// $(document).ready(function () {
+//   var container = document.querySelector('.tab-container')
+//   tabs(container);
+//   new ClipboardJS('.copy');
+// });
 
 /////////////////
 // Create Keys //
 /////////////////
 
-$(() => {
-    $('#generate').bind('click', function () {
+
+
+const crypto = Crypto();
+
+if (document.querySelector('#generate')){
+  document.querySelector('#generate').addEventListener('click', function(e){
+    e.preventDefault();
     var keyMnemPair = generatePrivateKeyFromMnemonic();
-    const seed = keyMnemPair[1];
-    $('#seed-output').text(seed);
-    const privateKey = keyMnemPair[0];
-    $('#privkey-output').text(privateKey)
-    makeqr($('#private-qr'), privateKey);
-    const publicKey = crypto.returnPublicKey(privateKey);
-    $('#pubkey-output').text(publicKey)
-    makeqr($('#public-qr'), publicKey);
-  })
-
-  $('#print').bind('click', function() {
-    //printJS('keys', 'html');
-    window.print();
-  })
-
-  $('#text-input').focus() // focus input box
-
-  //////////////////
-  // Recover Keys //
-  //////////////////
-
-  $('#seed-input').bind('input propertychange', function () {
-    const text = this.value;
-    if (text.trim().split(/\s+/g).length == 12) {
-      var recPrivateKey = mnemonicToSeed32(text).toString('hex');
-      $('#privateKey-recovered').text(recPrivateKey)
-      makeqr($('#private-qr-recovered'), recPrivateKey);
-    } else {
-      $('#privateKey-recovered').text("Seed Invalid");
-      $('#private-qr-recovered').html("");
-    }
+      const seed = keyMnemPair[1];
+      document.querySelector('#seed-output').innerText = seed;
+      const privateKey = keyMnemPair[0];
+      document.querySelector('#privkey-output').innerText = privateKey;
+      makeqr(document.querySelector('#private-qr'), privateKey);
+      console.log("privateKey: ", privateKey);
+      const publicKey = crypto.returnPublicKey(privateKey.toString('hex'));
+      document.querySelector('#pubkey-output').text(publicKey)
+      makeqr(document.querySelector('#public-qr'), publicKey);
   });
-})
+}
+
+document.querySelector('#print').addEventListener('click', function(e){
+    e.preventDefault();
+    window.print();
+});
+
+document.querySelectorAll('.copy').forEach(function(icon, i){
+   icon.addEventListener('click', function(e){
+      e.preventDefault();
+      let querySelector = e.currentTarget.getAttribute('data-clipboard-target');
+      console.log('querySelector: ', querySelector);
+      let value = document.querySelector(querySelector).innerText;
+      console.log('value: ', value);
+      navigator.clipboard.writeText(value);
+      alert('Copied to clipboard');
+  });
+});
+
+// $(() => {
+  
+//   $('#text-input').focus() // focus input box
+
+//   //////////////////
+//   // Recover Keys //
+//   //////////////////
+
+//   $('#seed-input').bind('input propertychange', function () {
+//     const text = this.value;
+//     if (text.trim().split(/\s+/g).length == 12) {
+//       var recPrivateKey = mnemonicToSeed32(text).toString('hex');
+//       $('#privateKey-recovered').text(recPrivateKey)
+//       makeqr($('#private-qr-recovered'), recPrivateKey);
+//     } else {
+//       $('#privateKey-recovered').text("Seed Invalid");
+//       $('#private-qr-recovered').html("");
+//     }
+//   });
+// })
 
 function makeqr(obj, data) {
   var typeNumber = 0;
-  var errorCorrectionLevel = 'H';
+  var errorCorrectionLevel = 'L';
   var qr = qrcode(typeNumber, errorCorrectionLevel);
   qr.addData(data);
   qr.make();
-  obj.html("");
-  obj.append(qr.createImgTag(5, 0, data));
+  obj.innerHTML = "";
+  let qrImg = qr.createImgTag(5, 0, data);
+  console.log("makeqr: ", qrImg);
+  document.querySelector("#private-qr").innerHTML = qrImg;
 }
 
 function generatePrivateKeyFromMnemonic() {
